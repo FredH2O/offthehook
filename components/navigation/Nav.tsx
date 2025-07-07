fad53622ea9d4b357c48ce26d73ac88c80775422
Fred <huertaserick@yahoo.ie>
2025-07-07 21:21:28 +0100

    feat: Navigation added, Link used from nextjs and Image from nextjs

---
+++ components/navigation/Nav.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navigation = ["Home", "Poll", "About"];
+
+const Nav = () => {
+  return (
+    <nav className="fixed top-0 w-full flex justify-evenly items-center">
+      <div className="flex items-center">
+        <Image
+          className="p-3"
+          src="/campfire.png"
+          alt="offthehook Logo"
+          width={100}
+          height={100}
+        />
+        <h2 className="text-2xl italic font-extralight">offthehook</h2>
+      </div>
+
+      <div>
+        <ul className="flex gap-3 ">
+          {Navigation.map((index) => (
+            <li
+              key={index}
+              className="hover:bg-red-500 px-3 py-1 rounded-sm transition-all duration-150"
+            >
+              <Link href="">{index}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
