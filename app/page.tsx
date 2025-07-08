763c6398741235586c2e8632eeae1a7ada761aff
Fred <huertaserick@yahoo.ie>
2025-07-08 18:35:16 +0100

    feat: added marquee for top news etc. will decide to keep or not

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Hero from "@/components/hero/Hero";
 import Nav from "@/components/navigation/Nav";
+import ScrollVelocity from "@/components/reactbits-components/ScrollVelocity";
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <Hero />
+      <ScrollVelocity
+        texts={[
+          "No Mither Gang, Unite!",
+          "Killer or Survivor: Who’s Got the Edge?",
+        ]}
+        velocity={20}
+        className="custom-scroll-text text-5xl"
+      />
     </div>
   );
 }
