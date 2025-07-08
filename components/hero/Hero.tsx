6e3cdf4384a577d79629c6c76e026c87ab164e93
Fred <huertaserick@yahoo.ie>
2025-07-08 18:15:18 +0100

    feat: fixed animation that includes from react-bits

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,4 +1,10 @@
+"use client";
 import Image from "next/image";
+import BlurText from "../reactbits-components/BlurText";
+
+const handleAnimationComplete = () => {
+  console.log("Animation completed.");
+};
 
 const Hero = () => {
   return (
@@ -16,9 +22,16 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/30 backdrop-blur-sm" />
 
       {/* Foreground content (optional) */}
-      <div className="relative z-10 flex flex-col justify-center items-center h-full text-white">
-        <h1 className="text-5xl font-bold font-bebas tracking-widest uppercase">
-          Off The Hook
+      <div className="relative z-10 flex flex-col container m-auto justify-center items-center h-full text-white">
+        <h1>
+          <BlurText
+            text="Off The Hook"
+            delay={150}
+            animateBy="words"
+            direction="top"
+            onAnimationComplete={handleAnimationComplete}
+            className="text-5xl font-bold font-bebas tracking-widest uppercase"
+          />
         </h1>
         <p className="text-lg mt-4 font-light">
           Where survivors chat by the fire.
