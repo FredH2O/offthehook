369d97f2582af7ac32a607e52ace8783281eff8d
Fred <huertaserick@yahoo.ie>
2025-07-08 19:37:25 +0100

    feat: structure for build of the week

---
+++ components/build-of-the-week/BuildOfTheWeek.tsx
@@ -0,0 +1,57 @@
+const BuildOfTheWeek = () => {
+  return (
+    <section>
+      {/* killer */}
+
+      <div>
+        <div>
+          <ul>
+            <li>Fire Up</li>
+            <li>Brutal Strength</li>
+            <li>Nowhere to Hide</li>
+            <li>Bamboozle</li>
+          </ul>
+        </div>
+        <div>
+          <p>
+            This build specialises in vaulting, kicking generators to see
+            survivors, ending chase time by almost 80%, pros of this build is
+            you can chase survivors fast, find them fast just by looking for
+            generators being done. cons is that at the start of the trial
+            survivors can do gens straight away without corrupt intervention,
+            not much slow down, and can only kick generators a certain amount of
+            time. overall this build is good for almost every killer in the game
+            specially M1 killers.
+          </p>
+        </div>
+      </div>
+      {/* survivor */}
+
+      <div>
+        <div>
+          <ul>
+            <li>Finesse</li>
+            <li>Kindred</li>
+            <li>Resilience</li>
+            <li>Lithe</li>
+          </ul>
+        </div>
+        <div>
+          <p>
+            This build specialises in chase time, squeezing every little seconds
+            you can from a killer so that your team mates can do generators.
+            Lithe for running away after a vault, Resilience speeds up your
+            vaulting and Finesse for that fast vault at the start. pros is if
+            your good with loops and vaulting then this build can be lethal even
+            when youre injured. cons is that if your injured finesse wont work
+            and resilience doesnt work when youre healthy. but this build
+            balances everything against almost every situation. Kindred is our
+            main aura reading unless its a stealth killer.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BuildOfTheWeek;
