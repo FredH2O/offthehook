eeeea322330392489b614678dc45799a52d4b9e6
Fred <huertaserick@yahoo.ie>
2025-07-10 20:02:31 +0100

    feat: added randomize button for perks

--- components/survivor-random-perk/SurvivorRandomPerk.tsx
+++ components/survivor-random-perk/SurvivorRandomPerk.tsx
@@ -1,25 +1,64 @@
+"use client";
 import Image from "next/image";
 import { survivorPerks } from "@/data/survivorPerks";
+import { useState } from "react";
 
-const getRandomSurvivorPerk = () => {
-  const index = Math.floor(Math.random() * survivorPerks.length);
-  return survivorPerks[index];
+const getRandomSurvivorPerk = (count: number) => {
+  const selected: string[] = [];
+
+  while (selected.length < count) {
+    const perk =
+      survivorPerks[Math.floor(Math.random() * survivorPerks.length)];
+    if (!selected.includes(perk)) {
+      selected.push(perk);
+    }
+  }
+
+  return selected;
 };
 
+export type Perks = string[];
+
 const SurvivorRandomPerk = () => {
-  const randomPerk1 = getRandomSurvivorPerk();
+  const [perks, setPerks] = useState<Perks>([]);
+
+  const handleRandomize = () => {
+    setPerks(getRandomSurvivorPerk(4));
+  };
 
   return (
-    <div>
-      <h2 className="">Survivor Random Perk</h2>
-      <div className="border">
-        <Image
-          src={`/survivor-perks-logo/${randomPerk1}.webp`}
-          width={100}
-          height={100}
-          alt="Random Survivor Perk"
-        />
+    <div className="flex flex-col justify-center items-center space-y-5">
+      <h2 className="text-4xl text-center">🎲 Survivor Random Perk</h2>
+
+      <div className="grid grid-cols-2 justify-center items-center gap-1 p-1">
+        {perks.map((perk, index) => (
+          <div
+            key={index}
+            className="flex flex-col w-full justify-center items-center px-3 py-2"
+          >
+            <Image
+              priority
+              src={`/survivor-perks-logo/${perk}.webp`}
+              width={100}
+              height={100}
+              alt="Random Survivor Perk"
+            />
+            <div>
+              <p className="px-3 py-1 rounded-sm bg-green-500/40 text-sm">
+                {perk.slice(0, 1).toUpperCase() +
+                  perk.replace(/_/g, " ").slice(1)}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
+
+      <button
+        onClick={handleRandomize}
+        className="px-3 py-1 border hover:cursor-pointer transition-all duration-75 active:translate-y-0.5 text-black active:bg-blue-500/50 bg-blue-500 rounded-sm"
+      >
+        Randomize My Perks
+      </button>
     </div>
   );
 };
