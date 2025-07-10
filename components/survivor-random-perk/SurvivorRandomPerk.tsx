65bcb81d0ad78d7eee57d0268c4a3698cbab156a
Fred <huertaserick@yahoo.ie>
2025-07-10 20:27:18 +0100

    feat: perk randomizer added with timeout and loading

--- components/survivor-random-perk/SurvivorRandomPerk.tsx
+++ components/survivor-random-perk/SurvivorRandomPerk.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { survivorPerks } from "@/data/survivorPerks";
 import { useState } from "react";
+import Loading from "../uiverse-component/Loading";
 
 const getRandomSurvivorPerk = (count: number) => {
   const selected: string[] = [];
@@ -21,41 +22,52 @@ export type Perks = string[];
 
 const SurvivorRandomPerk = () => {
   const [perks, setPerks] = useState<Perks>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const handleRandomize = () => {
-    setPerks(getRandomSurvivorPerk(4));
+    setLoading(true);
+
+    setTimeout(() => {
+      setPerks(getRandomSurvivorPerk(4));
+      setLoading(false);
+    }, 1500);
   };
 
   return (
     <div className="flex flex-col justify-center items-center space-y-5">
       <h2 className="text-4xl text-center">🎲 Survivor Random Perk</h2>
 
-      <div className="grid grid-cols-2 justify-center items-center gap-1 p-1">
-        {perks.map((perk, index) => (
-          <div
-            key={index}
-            className="flex flex-col w-full justify-center items-center px-3 py-2"
-          >
-            <Image
-              priority
-              src={`/survivor-perks-logo/${perk}.webp`}
-              width={100}
-              height={100}
-              alt="Random Survivor Perk"
-            />
-            <div>
-              <p className="px-3 py-1 rounded-sm bg-green-500/40 text-sm">
-                {perk.slice(0, 1).toUpperCase() +
-                  perk.replace(/_/g, " ").slice(1)}
-              </p>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="grid grid-cols-2 justify-center items-center gap-1 p-1">
+          {perks.map((perk, index) => (
+            <div
+              key={index}
+              className="flex flex-col w-full justify-center items-center px-3 py-2"
+            >
+              <Image
+                priority
+                src={`/survivor-perks-logo/${perk}.webp`}
+                width={100}
+                height={100}
+                alt="Random Survivor Perk"
+              />
+              <div>
+                <p className="px-3 py-1 rounded-sm bg-green-500/40 text-sm">
+                  {perk.slice(0, 1).toUpperCase() +
+                    perk.replace(/_/g, " ").slice(1)}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
 
       <button
+        disabled={loading}
         onClick={handleRandomize}
-        className="px-3 py-1 border hover:cursor-pointer transition-all duration-75 active:translate-y-0.5 text-black active:bg-blue-500/50 bg-blue-500 rounded-sm"
+        className="px-3 py-1 disabled:bg-gray-500 disabled:line-through border hover:cursor-pointer transition-all duration-75 active:translate-y-0.5 text-black active:bg-blue-500/50 bg-blue-500 rounded-sm"
       >
         Randomize My Perks
       </button>
